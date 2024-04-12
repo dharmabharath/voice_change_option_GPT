daf3dcb29cf4eb8c4cd834782b83dad9bb62e437
Dharma Bharath M <dharmabharath.m@quadrasystems.net>
2024-04-12 17:33:10 +0530

    msg1

--- hello_azure/views.py
+++ hello_azure/views.py
@@ -9,35 +9,23 @@ import os
 # import pyaudio
 # import wave
 
-# output_directory =r"static/audio"
-# os.makedirs(output_directory, exist_ok=True)
-
-# stop_speech_synthesis = False
-# speech_config = speechsdk.SpeechConfig(subscription="49a5b50e9b5d435eab3fbc6ffb1d11fe", region="eastus")
-# file_name = "outputaudio5.mp3"
-# full_file_path = os.path.join(output_directory, file_name)
-
-# audio_output_config = speechsdk.audio.AudioOutputConfig(filename=full_file_path)
-# speech_config.speech_synthesis_voice_name='en-US-JennyMultilingualNeural'
-# speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config, audio_config=audio_output_config)
-# stop_playback = False
-
-
-
+output_directory =r"static/audio"
+os.makedirs(output_directory, exist_ok=True)
 
 stop_speech_synthesis = False
 speech_config = speechsdk.SpeechConfig(subscription="49a5b50e9b5d435eab3fbc6ffb1d11fe", region="eastus")
+file_name = "outputaudio5.mp3"
+full_file_path = os.path.join(output_directory, file_name)
 
-
-audio_output_config = speechsdk.audio.AudioOutputConfig(use_default_speaker=True)
+audio_output_config = speechsdk.audio.AudioOutputConfig(filename=full_file_path)
 speech_config.speech_synthesis_voice_name='en-US-JennyMultilingualNeural'
 speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config, audio_config=audio_output_config)
 stop_playback = False
 
+@csrf_exempt
 def one(request):
     print("start")
     return render(request,'index.html')
-
 @csrf_exempt
 def ask_openai(request):
     global stop_playback
@@ -59,8 +47,8 @@ def ask_openai(request):
             {"role": "user", "content": request.POST['send']}
         ])
         collected_messages = []
-        last_tts_request = None
-
+        last_tts_request = ""
+        last_tts_Response=None
         # iterate through the stream response stream
         try:
             for chunk in response:                
@@ -71,90 +59,23 @@ def ask_openai(request):
                         if chunk_message in tts_sentence_end: # sentence end found
                             text = ''.join(collected_messages).strip() # join the recieved message together to build a sentence
                             if text != '' and stop_speech_synthesis!=True: # if sentence only have \n or space, we could skip
-                                print(f"Speech synthesized to speaker for: {text}")     
-                                last_tts_request = speech_synthesizer.speak_text_async(text)                            
-                                # result = speech_synthesizer.speak_text(text)                                
+                                print(f"Speech synthesized to speaker for: {text}")
+                                
+                                # result = speech_synthesizer.speak_text(text)
+                                last_tts_request +=text
+                                
                                 collected_messages.clear()
-            
+            last_tts_Response = speech_synthesizer.speak_text_async(last_tts_request) 
                                 
         except Exception as e:
             print("Erroe",e)
-        if last_tts_request:       
-            last_tts_request.get()     
-
+        if last_tts_Response:            
+            with open(full_file_path, 'wb') as file:
+                file.write(last_tts_Response.get().audio_data)
         # print("setseteeste",audio_data_list)
         return JsonResponse({'message':"success"}, status=200)
 
     return JsonResponse({'error': 'Invalid request method'}, status=400)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# def ask_openai(request):
-#     global stop_playback
-#     stop_playback = False
-#     audio_data_list = []
-#     print("saghd",request.POST['send'])
-#     if request.method=='POST':
-#         client = AzureOpenAI(
-#                 azure_endpoint="https://casggpt-4.openai.azure.com/",
-#                 api_key="62f99aff986e40a1918058887856b41d",
-#                 api_version="2023-05-15"
-#                 )
-#         deployment_id="gpt-35-turbo-16k"
-#         tts_sentence_end = [ ".", "!", "?", ";", "。", "！", "？", "；", "\n" ]
- 
-      
-#         # Ask Azure OpenAI in streaming way
-#         response = client.chat.completions.create(model=deployment_id, max_tokens=200, stream=True, messages=[
-#             {"role": "user", "content": request.POST['send']}
-#         ])
-#         collected_messages = []
-#         last_tts_request = ""
-#         last_tts_Response=None
-#         # iterate through the stream response stream
-#         try:
-#             for chunk in response:                
-#                 if len(chunk.choices) > 0:
-#                     chunk_message = chunk.choices[0].delta.content  # extract the message
-#                     if chunk_message is not None:
-#                         collected_messages.append(chunk_message)  # save the message
-#                         if chunk_message in tts_sentence_end: # sentence end found
-#                             text = ''.join(collected_messages).strip() # join the recieved message together to build a sentence
-#                             if text != '' and stop_speech_synthesis!=True: # if sentence only have \n or space, we could skip
-#                                 print(f"Speech synthesized to speaker for: {text}")
-                                
-#                                 # result = speech_synthesizer.speak_text(text)
-#                                 last_tts_request +=text
-                                
-#                                 collected_messages.clear()
-#             last_tts_Response = speech_synthesizer.speak_text_async(last_tts_request) 
-                                
-#         except Exception as e:
-#             print("Erroe",e)
-#         if last_tts_Response:            
-#             with open(full_file_path, 'wb') as file:
-#                 file.write(last_tts_Response.get().audio_data)
-#         # print("setseteeste",audio_data_list)
-#         return JsonResponse({'message':"success"}, status=200)
-
-#     return JsonResponse({'error': 'Invalid request method'}, status=400)
 
 
 #stop speech listening
