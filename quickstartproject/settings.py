517fe63a010dfbf7b0e69d6b2a0500c93e742559
Dharma Bharath M <dharmabharath.m@quadrasystems.net>
2024-05-24 17:56:28 +0530

    changed local storage fun

--- quickstartproject/settings.py
+++ quickstartproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '1234567890'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-ALLOWED_HOSTS = ['speechai.azurewebsites.net']
+ALLOWED_HOSTS = ['voicegpt.azurewebsites.net']
 
 # DEBUG = True
 # ALLOWED_HOSTS = []
